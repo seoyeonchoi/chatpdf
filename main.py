17c1ac70afaed77eec9ace510867419bb97db7c2
seoyeonchoi <choi.seoyeon@kt.com>
2024-03-08 16:50:29 +0900

    feat; stream 기능 추가

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
+from uuid import UUID
+from langchain_core.outputs import ChatGenerationChunk, GenerationChunk
 # from dotenv import load_dotenv
 # load_dotenv()
 
@@ -10,6 +12,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings.sentence_transformer import (
     SentenceTransformerEmbeddings,
 )
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_openai import ChatOpenAI
@@ -20,6 +23,7 @@ import os
 from streamlit_extras.buy_me_a_coffee import button
 
 button(username="Seoyeon", floating=True, width=221)
+
 
 # 제목
 st.title("ChatPDF")
@@ -59,7 +63,7 @@ if uploaded_file is not None:
 
     # create the open-source embedding function
     # embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-    embedding_function = OpenAIEmbeddings(openai_api_key=openai_key)  
+    embedding_function = OpenAIEmbeddings(openai_api_key=openai_key)
 
     # load it into Chroma (text to embedding model)
     db = Chroma.from_documents(texts, embedding_function)
@@ -68,9 +72,21 @@ if uploaded_file is not None:
     st.header("PDF에게 질문해보세요!!")
     question = st.text_input('질문을 입력하세요')
 
+    # Stream 받아 줄 handler 만들기
+    from langchain.callbacks.base import BaseCallbackHandler
+    class StreamHandler(BaseCallbackHandler):
+        def __init__(self, container, initial_text=""):
+            self.container = container
+            self.text=initial_text
+        def on_llm_new_token(self, token: str, **kwargs) -> None:
+            self.text+= token
+            self.container.markdown(self.text)
+
     if st.button('PDF에게 질문하기'):
         with st.spinner('Wait for it...'):
-            llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, max_tokens=500, openai_api_key=openai_key)  # 라마 교체 가능
+            chat_box = st.empty()
+            stream_handler = StreamHandler(chat_box)
+            llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, max_tokens=500, openai_api_key=openai_key, streaming=True, callbacks=[stream_handler])  # 라마 교체 가능
             qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
-            result = qa_chain({"query": question})
-            st.write(result["result"])
+            qa_chain({"query": question})
+            
