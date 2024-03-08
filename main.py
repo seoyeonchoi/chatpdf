a6e110e8be6116e103fe15a8451be47fb301355d
seoyeonchoi <choi.seoyeon@kt.com>
2024-03-08 15:10:17 +0900

    fix=file_uploader type 제한

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ st.title("ChatPDF")
 st.write("---")
 
 # 파일 업로드
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Choose a file", type=['pdf'])
 st.write("---")
 
 def pdf_to_document(uploaded_file):
